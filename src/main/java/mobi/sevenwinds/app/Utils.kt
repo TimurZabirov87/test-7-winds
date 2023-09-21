5e4230a7e407ccafc28f0f478dc3cbab55646324
TimurZabirov87 <94556981+TimurZabirov87@users.noreply.github.com>
2023-09-21 13:16:59 +0300

    tasks solving

--- src/main/java/mobi/sevenwinds/app/Utils.kt
+++ src/main/java/mobi/sevenwinds/app/Utils.kt
@@ -4,6 +4,7 @@ import org.joda.time.DateTime
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
+
 fun DateTime.toLDT(): LocalDateTime {
     return LocalDateTime.ofEpochSecond(this.millis, 0, ZoneOffset.UTC)
 }
